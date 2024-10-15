859a030984670d85f7f1dee6a3c45c649f640275
Mamadou Saliou DIALLO <kewere097@gmail.com>
2024-10-15 12:52:50 +0000

    first commit

---
+++ code.js
@@ -0,0 +1,19 @@
+for(let i = 1; i <= 10; i++){
+    console.log(i);
+}
+
+//  boucle while   
+
+let i = 1;
+while (i <= 10){
+    console.log(i);
+    i++;
+}
+
+// fruits 
+
+let fruits = ["Mangue", "Orange", "Banane", "Avocat", "Ananas"];
+for(i=0; i <= fruits.length; i++)
+{
+    console.log(fruits[i]);
+}
